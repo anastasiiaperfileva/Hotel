947c5172ea17da03c05dd7ece2f7048b3a45e743
anastasiiaperfileva <nastyaperfileva4160@gmail.com>
2025-11-07 18:52:45 +0300

    add configurator

---
+++ Hotel.Service/IoC/DbContextConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using Hotel.Service.Settings;
+using Hotel.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Service.IoC;
+
+public class DbContextConfigurator
+{
+    public static void ConfigureService(IServiceCollection services, HotelSettings settings)
+    {
+        services.AddDbContextFactory<HotelDbContext>(
+            options => { options.UseNpgsql(settings.HotelDbContextConnectionString); },
+            ServiceLifetime.Scoped);
+    }
+
+    public static void ConfigureApplication(IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<HotelDbContext>>();
+        using var context = contextFactory.CreateDbContext();
+        context.Database.Migrate(); 
+    }
+}
