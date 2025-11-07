1b715eb772ba9c49d79e588b685620a400e3e5cb
anastasiiaperfileva <nastyaperfileva4160@gmail.com>
2025-11-07 18:08:03 +0300

    app rattern repository

---
+++ Hotel.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿﻿using System.Linq.Expressions;
+ using Hotel.DataAccess.Entities;
+ 
+namespace Hotel.DataAccess.Repositories;
+
+public interface IRepository<T> where T: BaseEntity
+{
+      IEnumerable<T> GetAll();
+      IEnumerable<T> GetAll(Expression<Func<T, bool>> filter);
+      T? GetById(int id);
+      T? GetById(Guid id);
+      T Save(T entity);
+      void Delete(T entity);
+}
