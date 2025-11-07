aed59c602f4874583eee295e336c4c41e515f5ac
anastasiiaperfileva <nastyaperfileva4160@gmail.com>
2025-11-07 15:49:48 +0300

    update entities

--- Hotel.DataAccess/Entities/RoomStatusEntity.cs
+++ Hotel.DataAccess/Entities/RoomStatusEntity.cs
@@ -7,4 +7,5 @@ public class RoomStatusEntity: BaseEntity
 {
     public string Name { get; set; }
     public virtual ICollection<TypeOfRoomEntity> TypeOfRooms { get; set; }
+    public virtual ICollection<RoomEntity> Rooms { get; set; }
 }
